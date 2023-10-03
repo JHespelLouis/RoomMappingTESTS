6572da5cde9a556431bd84ab68557cfb2be64bb2
Bonheure Antoine <he202146@students.ephec.be>
2023-10-03 20:17:20 +0200

    Première ébauche et test du Display du Radar
    ajout de la librairie Victory
    test avec des données préalablement encodé

--- frontend/Components/Main.js
+++ frontend/Components/Main.js
@@ -1,10 +1,12 @@
 import { Component} from "react";
 import { Text, View } from "react-native";
 
+import DisplayRadar from "./Main/DisplayRadar";
+
 class Main extends Component{
     render(){
         return(
-            <Text>Main</Text>
+            <DisplayRadar/>
         )
     }
 
