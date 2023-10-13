1ebeb9ed98b76f1ea783930a2cb48d672f6fba4d
jclaerhout <j.claerhout@students.ephec.be>
2023-10-13 08:43:59 +0200

    (cleanup) branch : delete 'robot' and 'backend' repositories from this branch

--- Frontend-user-interface/screens/Historical.js
+++ Frontend-user-interface/screens/Historical.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
+import MapList from '../screens/MapList';
+
 function Historical() {
   return (
     <View style={styles.container}>
-      <Text>Historical</Text>
+      <MapList />
     </View>
   )
 }
