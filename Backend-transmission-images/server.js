3170df603c4831d572b6481b7cdb743bdcff2db1
e.cockburn@students.ephec.be <116933854+Euan-lc@users.noreply.github.com>
2023-10-30 18:33:37 +0100

    firebase integration
    first steps to intregrate firebase to nodejs

--- Backend-transmission-images/server.js
+++ Backend-transmission-images/server.js
@@ -1,4 +1,21 @@
 const express = require("express")
+require('dotenv').config()
+var admin = require("firebase-admin");
+const {getFirestore} = require("firebase-admin/firestore")
+console.log(process.env.SERVICE_ACCOUNT)
+var serviceAccount = require(process.env.SERVICE_ACCOUNT);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://roommapping-airsoft-default-rtdb.europe-west1.firebasedatabase.app"
+});
+
+const db = getFirestore()
+
+async function fetch(){
+    console.log(await db.collection("maps").doc("JoCeQlYFT3Mg7hJFone6").get())
+}
+
 const app = express()
 
-app.listen(5000, () => {console.log("server started on port 5000")})
+app.listen(5000, () => {console.log("server started on port 5000"); fetch()})
