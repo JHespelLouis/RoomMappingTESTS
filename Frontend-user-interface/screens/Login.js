391638df539139e9898d1a4f0e96355ad522bfc3
jclaerhout <j.claerhout@students.ephec.be>
2023-11-01 15:44:21 +0100

    Commit Message:
    Implemented user registration using Firebase Authentication
    
    This commit introduces the ability to create an account on the RoomMapping web application through Firebase Authentication.
    During the registration process, email and password formats are validated.
    Additionally, the system checks if an account with the provided email already exists.
    If all the input fields in the form are correct, the account is created in the Firebase project.
    A brief animation notifies the user of the successful account creation, and the user is then redirected to the app's login page.
    
    This feature enhances user onboarding and provides a seamless registration experience.

--- Frontend-user-interface/screens/Login.js
+++ Frontend-user-interface/screens/Login.js
@@ -1,16 +1,6 @@
-import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
+import React, { useState } from 'react';
+import { Avatar, Button, CssBaseline, TextField, FormControlLabel, Checkbox, Link, Grid, Box, Typography, Container } from '@mui/material'
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import { useNavigation } from '@react-navigation/native';
@@ -68,10 +58,6 @@ export default function Login() {
               type="password"
               id="password"
               autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Se souvenir de moi"
             />
             <Button
               type="submit"
