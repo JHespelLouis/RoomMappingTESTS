7c654fc4513abafab2805d326b169c9be1d39a6b
Euan-lc <116933854+Euan-lc@users.noreply.github.com>
2023-11-13 15:45:18 +0100

    Added publish dialog popup
    added the publish dialog popup, it consists of a radio group which conditionally renders divs which will contain the appropriate menu for handling the publishing of each type of map. The current divs are just placeholders

--- Frontend-user-interface/src/pages/MapList.js
+++ Frontend-user-interface/src/pages/MapList.js
@@ -7,112 +7,171 @@ import IconButton from '@mui/material/IconButton';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { grey } from '@mui/material/colors';
+import {grey} from '@mui/material/colors';
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import {FormControl, FormControlLabel, FormLabel, Radio, RadioGroup} from "@mui/material";
 
 function MapOptions(...props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  return(
-      <div>
-        <IconButton
-            id="basic-button"
-            aria-controls={open ? 'basic-menu' : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? 'true' : undefined}
-            onClick={handleClick}
-        >
-          <MoreVertIcon sx={{color: grey[50]}}/>
-        </IconButton>
-        <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              'aria-labelledby': 'basic-button',
-            }}
-        >
-          <MenuItem onClick={()=>{console.log('modifying');handleClose()}}>Modifier</MenuItem>
-          <MenuItem onClick={handleClose}>Supprimer</MenuItem>
-          <MenuItem onClick={handleClose}>Publier</MenuItem>
-          <MenuItem onClick={()=>{handleClose();window.open(props[0]['img'], '_blank')}}>Agrandir</MenuItem>
-        </Menu>
-      </div>
-  )
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [publishOpen, setPublishOpen] = React.useState(false);
+    const open = Boolean(anchorEl);
+    const [popupContent, setPopupContent] = React.useState('statique')
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const handlePublish = () => {
+        setPublishOpen(true);
+        handleClose();
+    };
+    const handlePublishClose = () => {
+        setPublishOpen(false);
+    }
+    const handleRadioChange = (event: event) => {
+        setPopupContent(event.target.value);
+    };
+    const handleFinalPublish = () => {
+        console.log('placeholder function for publishing static map')
+    }
+    return (
+        <div>
+            <IconButton
+                id="basic-button"
+                aria-controls={open ? 'basic-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? 'true' : undefined}
+                onClick={handleClick}
+            >
+                <MoreVertIcon sx={{color: grey[50]}}/>
+            </IconButton>
+            <Menu
+                id="basic-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+            >
+                <MenuItem onClick={() => {
+                    console.log('modifying');
+                    handleClose()
+                }}>Modifier</MenuItem>
+                <MenuItem onClick={handleClose}>Supprimer</MenuItem>
+                <MenuItem onClick={handlePublish}>Publier</MenuItem>
+                <MenuItem onClick={() => {
+                    handleClose();
+                    window.open(props[0]['img'], '_blank')
+                }}>Agrandir</MenuItem>
+            </Menu>
+            <Dialog open={publishOpen} onClose={handlePublishClose}>
+                <DialogContent>
+                    <FormControl>
+                        <FormLabel>Type de map</FormLabel>
+                        <RadioGroup
+                            row
+                            value={popupContent}
+                            onChange={handleRadioChange}
+                        >
+                            <FormControlLabel value="statique" control={<Radio/>} label="statique"/>
+                            <FormControlLabel value="dynamique" control={<Radio/>} label="dynamique"/>
+                            <FormControlLabel value="group" control={<Radio/>} label="group"/>
+                        </RadioGroup>
+                    </FormControl>
+                    {popupContent === 'statique' && <div>
+                        <p>this will publish a static map</p>
+                        <Button onClick={handleFinalPublish}>Publish</Button>
+                    </div>}
+                    {popupContent === 'dynamique' && <div>
+                        <p>dynamique</p>
+                    </div>}
+                    {popupContent === 'group' && <div>
+                        <p>group</p>
+                    </div>}
+
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handlePublishClose}>Valider</Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    )
 }
 
 export default function MapList() {
-  return (
-      <ImageList sx={{ width: 700, height: 1000 }}>
-        <ImageListItem key="Subheader" cols={2}>
-          <ListSubheader component="div">Nombre de scans : {itemData.length}</ListSubheader>
-        </ImageListItem>
-        {itemData.map((item) => (
-            <ImageListItem key={item.img}>
-              <img
-                  src={item.img}
-                  srcSet={item.img}
-                  alt={item.title}
-                  loading="lazy"
-              />
-              <ImageListItemBar
-                  title={item.title}
-                  subtitle={item.author}
-                  actionIcon={
-                    <MapOptions img={item.img}/>
-                  }
-              />
+    return (
+        <ImageList sx={{width: 700, height: 1000}}>
+            <ImageListItem key="Subheader" cols={2}>
+                <ListSubheader component="div">Nombre de scans : {itemData.length}</ListSubheader>
             </ImageListItem>
-        ))}
-      </ImageList>
-  );
+            {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+                    <img
+                        src={item.img}
+                        srcSet={item.img}
+                        alt={item.title}
+                        loading="lazy"
+                    />
+                    <ImageListItemBar
+                        title={item.title}
+                        subtitle={item.author}
+                        actionIcon={
+                            <MapOptions img={item.img}/>
+                        }
+                    />
+                </ImageListItem>
+            ))}
+        </ImageList>
+    );
 }
 
 const itemData = [
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 1',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 2',
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 3',
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 4',
-    cols: 2,
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 5',
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 6',
-  },
-  {
-    img: require('../assets/parcelle.png'),
-    title: 'Plan 7',
-  },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 1',
+        rows: 2,
+        cols: 2,
+        featured: true,
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 2',
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 3',
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 4',
+        cols: 2,
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 5',
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 6',
+    },
+    {
+        img: require('../assets/parcelle.png'),
+        title: 'Plan 7',
+    },
 ];
 
 const styles = {
-  container : {
-    flex : 1,
-    alignItems : 'center',
-    justifyContent : 'center',
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 };
