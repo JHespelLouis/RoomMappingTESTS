ffa7c8e56689e0357bd06550459e0270e20ac3dc
Slywoud <he202160@students.ephec.be>
2023-11-14 17:53:00 +0100

    GetObject from s3 working and get a more specific data from firebase
    Changed the s3 bucket and can get objects from the s3 bucket with getObjectsFromS3Bucket()

--- Backend-transmission-images/s3.js
+++ Backend-transmission-images/s3.js
@@ -3,7 +3,8 @@ const aws = require('aws-sdk')
 const crypto = require ('crypto')
 const { promisify } = require("util")
 const randomBytes = promisify(crypto.randomBytes)
-require('dotenv').config({path :'.env-local'});
+require('dotenv').config({path :'.env'});
+const {ListObjectsV2Command, S3Client} = require("@aws-sdk/client-s3");
 
 dotenv.config()
 
@@ -20,8 +21,8 @@ const s3 = new aws.S3({
 })
 
 async function generateUploadURL() {
-    const rawBytes = await randomBytes(16)
-    const imageName = rawBytes.toString('hex')
+   const rawBytes = await randomBytes(16)
+   const imageName = rawBytes.toString('hex')
 
     const params = ({
         Bucket: bucketName,
@@ -29,8 +30,66 @@ async function generateUploadURL() {
         Expires: 60
     })
 
-    const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+    const uploadURL = await s3.getSignedUrlPromise('getObject', params)
+    console.log(uploadURL)
     return uploadURL
 }
 
-module.exports = {generateUploadURL};
+async function getObject(){
+    const params = ({
+        Bucket: 'roommappingbucket',
+        Key: 'Cat.png'
+    })
+    s3.getObject(params, function(err, data){
+        if (err){console.log(err, err.stack)}
+        else console.log(data)
+    })
+}
+
+const client = new S3Client({});
+
+async function getKeys(){
+    const command = new ListObjectsV2Command({
+        Bucket: "roommappingBucket",
+        region: "eu-north-1"
+    });
+
+    try {
+        let isTruncated = true;
+
+        console.log("Your bucket contains the following objects:\n");
+        let contents = "";
+
+        while (isTruncated) {
+            const { Contents, IsTruncated, NextContinuationToken } =
+                await client.send(command);
+            const contentsList = Contents.map((c) => ` • ${c.Key}`).join("\n");
+            contents += contentsList + "\n";
+            isTruncated = IsTruncated;
+            command.input.ContinuationToken = NextContinuationToken;
+        }
+        console.log(contents);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function getObjectsFromS3Bucket() {
+    let isTruncated = true;
+    let marker;
+    while(isTruncated) {
+        let params = {Bucket: "roommappingbucket"};
+        if (marker) params.Marker = marker;
+        const response = await s3.listObjects(params).promise();
+        response.Contents.forEach(item => {
+            console.log(item.Key);
+            getObject(item.key)
+        });
+        isTruncated = response.IsTruncated
+        if (isTruncated) {
+            marker = response.Contents.slice(-1)[0].Key;
+        }
+    }
+}
+
+module.exports = {generateUploadURL, getObject, getKeys, getObjectsFromS3Bucket};
--- Backend-transmission-images/server.js
+++ Backend-transmission-images/server.js
@@ -4,6 +4,12 @@ var admin = require("firebase-admin");
 const {getFirestore} = require("firebase-admin/firestore")
 console.log(process.env.SERVICE_ACCOUNT)
 var serviceAccount = require(process.env.SERVICE_ACCOUNT);
+const AWS = require('aws-sdk');
+require('aws-sdk/lib/maintenance_mode_message').suppress = true;
+const {generateUploadURL, getObject, getKeys, upload, getAllObjectsFromS3Bucket, getObjectsFromS3Bucket} = require("./s3");
+//*/ get reference to S3 client
+var s3 = new AWS.S3();
+
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -17,20 +23,30 @@ async function fetch(){
 }
 
 async function readData() {
-    const snapshot = await db.collection("data").doc("maps").collection("user1").get();
+    const snapshot = await db.collection("data").doc("maps").collection("user2").get();
     const maps = [];
     snapshot.forEach(doc => {
         maps.push({ mapId: doc.id, ...doc.data() })
     })
-    console.log(maps)
-    return maps;
+    let name = maps.map((item) => item.nom)
+    console.log(name)
+    return name;
+}
+
+myBucket = 'roommappingbucket'
+
+async function readBucket() {
+    s3.listObjectsV2({Bucket: myBucket })
+        .promise()
+        .then(data => {
+            console.log(data);
+            const baseURL = 'https://s3.amazonaws.com/${myBucket}/';
+            let urlArr = data.Contents.map(e => baseURL + e.key);
+            console.log(urlArr)
+        })
+        .catch(err => console.log(err))
 }
 
 const app = express()
 
-app.get("/", async (req, res) => {
-    const data = await readData("data");
-    console.log(data)
-})
-
-app.listen(5000, () => {console.log("server started on port 5000"); readData()})
+app.listen(5000, () => {console.log("server started on port 5000"); getObjectsFromS3Bucket()})
